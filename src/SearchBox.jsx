bd876292defd8cedd9f12e9e2e8a63964d69ee82
Jay Vishwakarma <jayvishwakarma10125@gmail.com>
2024-08-06 12:52:05 +0530

    Updated files

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,12 +2,14 @@ import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 import "./SearchBox.css"
 import { useState } from "react"
+import React from "react"
 
 export default function SearchBox({updateInfo}){
     let [city, setCity] = useState("")
     let [error, setError] = useState(false)
-   const API_URL="http://api.openweathermap.org/data/2.5/weather"
-    const API_KEY="1031e82180981b48e6b9fba105594774"
+   const API_URL=process.env.REACT_APP_API_URL
+    const API_KEY=process.env.REACT_APP_API_KEY
+
 
     let getWeatherInfo = async ()=>{
       try{
