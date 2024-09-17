f2a1e2b3827db16bca081934ab56161bc82cfc52
Jay Vishwakarma <jayvishwakarma10125@gmail.com>
2024-09-17 22:21:36 +0530

    added

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,78 +1,3 @@
-// import TextField from "@mui/material/TextField";
-// import Button from "@mui/material/Button";
-// import "./SearchBox.css";
-// import { useState } from "react";
-// import React from "react";
-// import.meta.env;
-
-// export default function SearchBox({ updateInfo }) {
-//   let [city, setCity] = useState("");
-//   let [error, setError] = useState(false);
-//   const API_URL = import.meta.env.VITE_API_URL;
-//   const API_KEY = import.meta.env.VITE_API_KEY;
-
-//   let getWeatherInfo = async () => {
-//     try {
-//       let response = await fetch(
-//         `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
-//       );
-//       let jsonResponse = await response.json();
-//       console.log(jsonResponse);
-
-//       let result = {
-//         city: city,
-//         temp: jsonResponse.main.temp,
-//         tempMax: jsonResponse.main.temp_max,
-//         tempMin: jsonResponse.main.temp_min,
-//         humidity: jsonResponse.main.humidity,
-//         feelsLike: jsonResponse.main.feels_like,
-//         weather: jsonResponse.weather[0].description,
-//       };
-//       console.log(result);
-//       return result;
-//     } catch (err) {
-//       throw err;
-//     }
-//   };
-
-//   let handleChange = (evt) => {
-//     setCity(evt.target.value);
-//   };
-
-//   let handleSubmit = async (evt) => {
-//     try {
-//       evt.preventDefault();
-//       console.log(city);
-//       setCity("");
-//       let newInfo = await getWeatherInfo();
-//       updateInfo(newInfo);
-//     } catch (err) {
-//       setError(true);
-//     }
-//   };
-//   return (
-//     <div className="SearchBox">
-//       <form onSubmit={handleSubmit}>
-//         <TextField
-//           size="small"
-//           id="city"
-//           label="City Name"
-//           variant="outlined"
-//           value={city}
-//           onChange={handleChange}
-//           required
-//         ></TextField>
-//         <br />
-//         <br />
-//         <Button variant="contained" type="submit">
-//           Search
-//         </Button>
-//         {error && <p style={{ color: "red" }}>No such place exists!</p>}
-//       </form>
-//     </div>
-//   );
-// }
-
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
@@ -112,6 +37,7 @@ export default function SearchBox({ updateInfo }) {
 
   let handleChange = (evt) => {
     setCity(evt.target.value);
+    setError(false);
   };
 
   let handleSubmit = async (evt) => {
@@ -120,6 +46,7 @@ export default function SearchBox({ updateInfo }) {
       console.log(city);
       setCity("");
       let newInfo = await getWeatherInfo();
+      setError(false);
       updateInfo(newInfo);
     } catch (err) {
       setError(true);
@@ -137,6 +64,7 @@ export default function SearchBox({ updateInfo }) {
           onChange={handleChange}
           required
         ></TextField>
+
         <Button variant="contained" type="submit">
           Search
         </Button>
