6ed057981dfde8242442b5613c118fe77dbe0e26
Jay Vishwakarma <jayvishwakarma10125@gmail.com>
2024-09-17 16:59:55 +0530

    added

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,8 +8,10 @@ import.meta.env;
 export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
-  const API_URL = "REACT_APP_API_URL";
-  const API_KEY = "REACT_APP_API_KEY";
+  // const API_URL = "REACT_APP_API_URL";
+  // const API_KEY = "REACT_APP_API_KEY";
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   let getWeatherInfo = async () => {
     try {
